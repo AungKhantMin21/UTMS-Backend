2a012095d515eace8670595efb26b1ed3aa1eae4
Aung Khant Min <aungkhantmin212002@gmail.com>
2025-05-27 00:40:08 +0700

    improve signup to full feature (jwt,cookie,bcrypt,etc)

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,14 +1,33 @@
+const jwt = require('jsonwebtoken');
+
 const { createUserSchema } = require('../validators/userValidator');
 const prisma = require('../db');
+
+const signToken = (id) => {
+    return jwt.sign({id: id}, process.env.JWT_SECRET,{
+        expiresIn: process.env.JWT_EXPIRE_IN
+    });
+}
+
+const saveCookie = (token,res) => {
+    const cookieOptions = {
+        expires: new Date( Date.now() + (process.env.JWT_COOKIE_EXPIRE * 24 * 60 * 60 * 1000)),
+        httpOnly: true
+    }
+
+    res.cookie('jwt', token, cookieOptions);
+}
 
 exports.signup = async (req,res,next) => {
     try {
         const data = createUserSchema.parse(req.body);
-        
         const newUser = await prisma.user.create({ data });
 
+        const token = signToken(newUser.id);
+        saveCookie(token,res);
         res.status(201).json({
             status: 'success',
+            token: token,
             data: newUser
         });
     } catch(err) {
--- db.js
+++ db.js
@@ -1,14 +1,16 @@
 const { PrismaClient } = require('@prisma/client');
+const bcrypt = require('bcryptjs');
 
 const prisma = new PrismaClient();
 
 prisma.$use(async (params, next) => {
   if (params.model === 'User') {
     if (params.action === 'create') {
+        params.args.data.password = await bcrypt.hash(params.args.data.password, 12);
         params.args.data.passwordConfirm = undefined;
     }
   }
-  
+
   return next(params)
 })
 
