c54fb23bcd0a8f0e87da878254df26b898292513
Aung Khant Min <aungkhantmin212002@gmail.com>
2025-05-27 00:00:46 +0700

    added basic error handling

--- controllers/errorController.js
+++ controllers/errorController.js
@@ -1,14 +1,41 @@
+const AppError = require("../utils/AppError");
+
+const handleDuplicateErrorDB = (err) => {
+    const message = `This ${err.meta.target} already exists on ${err.meta.modelName} table`;
+    return new AppError(message, 409);
+}
+
+const handleValidationError = (err) => {
+    const formattedErrors = err.issues.map(e => (e.message));
+    return new AppError(formattedErrors.join(' and '), 500);
+}
+
 const sendError = (err,res) => {
     res.status(err.statusCode).json({
         status: err.status,
         message: err.message,
-        stack: err.stack
+        detail: err
     });
 }
 
 module.exports = (err,req,res,next) => {
     err.status = err.status || 'error';
     err.statusCode = err.statusCode || 500;
+    let error = JSON.parse(JSON.stringify(err));
 
-    sendError(err,res);
+        console.log(error);
+    if(error.name === 'PrismaClientKnownRequestError'){
+        // DUPLICATE DATA ERROR 
+        if(error.code === 'P2002'){
+            error = handleDuplicateErrorDB(error);
+        }
+    }
+
+    if(error.name === 'ZodError'){
+        // ZOD VALIDATION ERROR
+        error = handleValidationError(error);
+    }
+    
+
+    sendError(error,res);
 }
